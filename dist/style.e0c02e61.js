a4435560c4918b2408c2afe11606ec4ccb4f2970
Алексей Просвирнин <alexpro@MacBookAlexPRO.local>
2020-08-10 14:19:59 +0300

    Update everything from GDrive

--- dist/style.e0c02e61.js
+++ dist/style.e0c02e61.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+})({"C:/Users/Алексей/AppData/Roaming/npm/node_modules/parcel/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
 function getBundleURLCached() {
@@ -149,7 +149,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+},{}],"C:/Users/Алексей/AppData/Roaming/npm/node_modules/parcel/src/builtins/css-loader.js":[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -184,12 +184,17 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"style/style.scss":[function(require,module,exports) {
+},{"./bundle-url":"C:/Users/Алексей/AppData/Roaming/npm/node_modules/parcel/src/builtins/bundle-url.js"}],"font/stylesheet.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./GothamPro-Bold.eot":[["GothamPro-Bold.14e11ece.eot","font/GothamPro-Bold.eot"],"font/GothamPro-Bold.eot"],"./GothamPro-Bold.woff2":[["GothamPro-Bold.a4428282.woff2","font/GothamPro-Bold.woff2"],"font/GothamPro-Bold.woff2"],"./GothamPro-Bold.woff":[["GothamPro-Bold.fb3349e9.woff","font/GothamPro-Bold.woff"],"font/GothamPro-Bold.woff"],"./GothamPro-Bold.ttf":[["GothamPro-Bold.e036e46c.ttf","font/GothamPro-Bold.ttf"],"font/GothamPro-Bold.ttf"],"./GothamPro-Light.eot":[["GothamPro-Light.43673d97.eot","font/GothamPro-Light.eot"],"font/GothamPro-Light.eot"],"./GothamPro-Light.woff2":[["GothamPro-Light.b04db0cf.woff2","font/GothamPro-Light.woff2"],"font/GothamPro-Light.woff2"],"./GothamPro-Light.woff":[["GothamPro-Light.404c48c9.woff","font/GothamPro-Light.woff"],"font/GothamPro-Light.woff"],"./GothamPro-Light.ttf":[["GothamPro-Light.71258072.ttf","font/GothamPro-Light.ttf"],"font/GothamPro-Light.ttf"],"./GothamPro-Black.eot":[["GothamPro-Black.49d4a4b4.eot","font/GothamPro-Black.eot"],"font/GothamPro-Black.eot"],"./GothamPro-Black.woff2":[["GothamPro-Black.175d613f.woff2","font/GothamPro-Black.woff2"],"font/GothamPro-Black.woff2"],"./GothamPro-Black.woff":[["GothamPro-Black.6cc277be.woff","font/GothamPro-Black.woff"],"font/GothamPro-Black.woff"],"./GothamPro-Black.ttf":[["GothamPro-Black.9f2e017b.ttf","font/GothamPro-Black.ttf"],"font/GothamPro-Black.ttf"],"./GothamPro-Italic.eot":[["GothamPro-Italic.e51f045a.eot","font/GothamPro-Italic.eot"],"font/GothamPro-Italic.eot"],"./GothamPro-Italic.woff2":[["GothamPro-Italic.82087a4e.woff2","font/GothamPro-Italic.woff2"],"font/GothamPro-Italic.woff2"],"./GothamPro-Italic.woff":[["GothamPro-Italic.292314d7.woff","font/GothamPro-Italic.woff"],"font/GothamPro-Italic.woff"],"./GothamPro-Italic.ttf":[["GothamPro-Italic.73cd2a73.ttf","font/GothamPro-Italic.ttf"],"font/GothamPro-Italic.ttf"],"./GothamProNarrow-Bold.eot":[["GothamProNarrow-Bold.02bc055b.eot","font/GothamProNarrow-Bold.eot"],"font/GothamProNarrow-Bold.eot"],"./GothamProNarrow-Bold.woff2":[["GothamProNarrow-Bold.8020c97c.woff2","font/GothamProNarrow-Bold.woff2"],"font/GothamProNarrow-Bold.woff2"],"./GothamProNarrow-Bold.woff":[["GothamProNarrow-Bold.637790a7.woff","font/GothamProNarrow-Bold.woff"],"font/GothamProNarrow-Bold.woff"],"./GothamProNarrow-Bold.ttf":[["GothamProNarrow-Bold.279f02ab.ttf","font/GothamProNarrow-Bold.ttf"],"font/GothamProNarrow-Bold.ttf"],"./GothamPro-Medium.eot":[["GothamPro-Medium.9951d6e0.eot","font/GothamPro-Medium.eot"],"font/GothamPro-Medium.eot"],"./GothamPro-Medium.woff2":[["GothamPro-Medium.b322caac.woff2","font/GothamPro-Medium.woff2"],"font/GothamPro-Medium.woff2"],"./GothamPro-Medium.woff":[["GothamPro-Medium.c46225a2.woff","font/GothamPro-Medium.woff"],"font/GothamPro-Medium.woff"],"./GothamPro-Medium.ttf":[["GothamPro-Medium.a2aff262.ttf","font/GothamPro-Medium.ttf"],"font/GothamPro-Medium.ttf"],"./GothamPro-BoldItalic.eot":[["GothamPro-BoldItalic.84500262.eot","font/GothamPro-BoldItalic.eot"],"font/GothamPro-BoldItalic.eot"],"./GothamPro-BoldItalic.woff2":[["GothamPro-BoldItalic.ae96de69.woff2","font/GothamPro-BoldItalic.woff2"],"font/GothamPro-BoldItalic.woff2"],"./GothamPro-BoldItalic.woff":[["GothamPro-BoldItalic.311f7302.woff","font/GothamPro-BoldItalic.woff"],"font/GothamPro-BoldItalic.woff"],"./GothamPro-BoldItalic.ttf":[["GothamPro-BoldItalic.594c9234.ttf","font/GothamPro-BoldItalic.ttf"],"font/GothamPro-BoldItalic.ttf"],"./GothamPro.eot":[["GothamPro.f4875da3.eot","font/GothamPro.eot"],"font/GothamPro.eot"],"./GothamPro.woff2":[["GothamPro.5aacae16.woff2","font/GothamPro.woff2"],"font/GothamPro.woff2"],"./GothamPro.woff":[["GothamPro.bc773320.woff","font/GothamPro.woff"],"font/GothamPro.woff"],"./GothamPro.ttf":[["GothamPro.0af2f95e.ttf","font/GothamPro.ttf"],"font/GothamPro.ttf"],"./GothamPro-MediumItalic.eot":[["GothamPro-MediumItalic.2753804e.eot","font/GothamPro-MediumItalic.eot"],"font/GothamPro-MediumItalic.eot"],"./GothamPro-MediumItalic.woff2":[["GothamPro-MediumItalic.c7fe4dd3.woff2","font/GothamPro-MediumItalic.woff2"],"font/GothamPro-MediumItalic.woff2"],"./GothamPro-MediumItalic.woff":[["GothamPro-MediumItalic.e51cd056.woff","font/GothamPro-MediumItalic.woff"],"font/GothamPro-MediumItalic.woff"],"./GothamPro-MediumItalic.ttf":[["GothamPro-MediumItalic.e6996ea6.ttf","font/GothamPro-MediumItalic.ttf"],"font/GothamPro-MediumItalic.ttf"],"./GothamPro-BlackItalic.eot":[["GothamPro-BlackItalic.de7a88b8.eot","font/GothamPro-BlackItalic.eot"],"font/GothamPro-BlackItalic.eot"],"./GothamPro-BlackItalic.woff2":[["GothamPro-BlackItalic.c8caef04.woff2","font/GothamPro-BlackItalic.woff2"],"font/GothamPro-BlackItalic.woff2"],"./GothamPro-BlackItalic.woff":[["GothamPro-BlackItalic.72a4777f.woff","font/GothamPro-BlackItalic.woff"],"font/GothamPro-BlackItalic.woff"],"./GothamPro-BlackItalic.ttf":[["GothamPro-BlackItalic.51d57768.ttf","font/GothamPro-BlackItalic.ttf"],"font/GothamPro-BlackItalic.ttf"],"./GothamProNarrow-Medium.eot":[["GothamProNarrow-Medium.2fdb8524.eot","font/GothamProNarrow-Medium.eot"],"font/GothamProNarrow-Medium.eot"],"./GothamProNarrow-Medium.woff2":[["GothamProNarrow-Medium.c507e8a6.woff2","font/GothamProNarrow-Medium.woff2"],"font/GothamProNarrow-Medium.woff2"],"./GothamProNarrow-Medium.woff":[["GothamProNarrow-Medium.e03435b6.woff","font/GothamProNarrow-Medium.woff"],"font/GothamProNarrow-Medium.woff"],"./GothamProNarrow-Medium.ttf":[["GothamProNarrow-Medium.467b96ec.ttf","font/GothamProNarrow-Medium.ttf"],"font/GothamProNarrow-Medium.ttf"],"./GothamPro-LightItalic.eot":[["GothamPro-LightItalic.2e1325da.eot","font/GothamPro-LightItalic.eot"],"font/GothamPro-LightItalic.eot"],"./GothamPro-LightItalic.woff2":[["GothamPro-LightItalic.d2a6cb1e.woff2","font/GothamPro-LightItalic.woff2"],"font/GothamPro-LightItalic.woff2"],"./GothamPro-LightItalic.woff":[["GothamPro-LightItalic.9267d1f7.woff","font/GothamPro-LightItalic.woff"],"font/GothamPro-LightItalic.woff"],"./GothamPro-LightItalic.ttf":[["GothamPro-LightItalic.cb8b2d28.ttf","font/GothamPro-LightItalic.ttf"],"font/GothamPro-LightItalic.ttf"],"_css_loader":"C:/Users/Алексей/AppData/Roaming/npm/node_modules/parcel/src/builtins/css-loader.js"}],"style/style.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./../font/stylesheet.css":"font/stylesheet.css","_css_loader":"C:/Users/Алексей/AppData/Roaming/npm/node_modules/parcel/src/builtins/css-loader.js"}],"C:/Users/Алексей/AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +222,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57605" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50660" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -393,5 +398,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+},{}]},{},["C:/Users/Алексей/AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
 //# sourceMappingURL=/style.e0c02e61.js.map
